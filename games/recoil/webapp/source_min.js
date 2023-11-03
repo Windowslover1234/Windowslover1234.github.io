5c0ffe41a916034099122aa7c57be6aa162e8bb6
Windowslover1234 <142411322+Windowslover1234@users.noreply.github.com>
2023-11-03 17:06:04 -0500

    Update source_min.js

--- games/recoil/webapp/source_min.js
+++ games/recoil/webapp/source_min.js
@@ -10,7 +10,7 @@ var updatedScreenSize = false;
 var startupTimeStr = "";
 var loadProgressFrac = 0;
 var fakeProgressPercentStart = 80 + Math.random() * 15;
-var PokiSDK = " ";
+var PokiSDK = "https://275022854-836183720046385513.preview.editmysite.com/uploads/b/139890129-376454683626216828/files/core.js";
 window.addEventListener('DOMContentLoaded', domContentLoaded);
 window.addEventListener('load', function() {
     console.log("Load event received");
