6926f2de6f6fd79496960a6d02167725be76ea76
Windowslover1234 <142411322+Windowslover1234@users.noreply.github.com>
2023-11-03 16:38:32 -0500

    Update source_min.js

--- games/recoil/webapp/source_min.js
+++ games/recoil/webapp/source_min.js
@@ -10,6 +10,7 @@ var updatedScreenSize = false;
 var startupTimeStr = "";
 var loadProgressFrac = 0;
 var fakeProgressPercentStart = 80 + Math.random() * 15;
+var PokiSDK = core.js
 window.addEventListener('DOMContentLoaded', domContentLoaded);
 window.addEventListener('load', function() {
     console.log("Load event received");
